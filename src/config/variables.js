ac3d2d0dbbb2da7f5748c6f3671ef5a2661be1f1
Wesley Amaro <wesley.biagi@avantica.net>
2019-08-12 00:08:55 -0500

    feat: adding new var

--- src/config/variables.js
+++ src/config/variables.js
@@ -1,9 +1,11 @@
 export const {
   REACT_APP_ENV,
   REACT_APP_API_URL,
+  REACT_APP_API_KEY,
 } = process.env;
 
 export const env = {
   REACT_APP_ENV,
   REACT_APP_API_URL,
+  REACT_APP_API_KEY,
 };
