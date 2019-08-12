d0cd5e0db6d0d99d10432dfbd2af4fdb77f057da
Wesley Amaro <wesley.biagi@avantica.net>
2019-08-12 00:07:09 -0500

    feat: search

---
+++ src/components/Grid/Col/index.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// xs = mobile
+// sm = small tablet
+// md = tablet
+// lg = web
+
+const Col = (props) => {
+  const {
+    cellSpacing, // from row
+    rowSpacing, // from row
+    direction,
+    horizontalAlign,
+    verticalAlign,
+    className,
+    children,
+    show,
+    xs,
+    sm,
+    md,
+    lg,
+    ...rest
+  } = props;
+
+  if (show) {
+    return (
+      <div
+        className={`grid-col-xs-${xs} grid-col-sm-${sm} grid-col-md-${md} grid-col-lg-${lg} ${className}`}
+        style={{
+          padding: `${rowSpacing}px ${cellSpacing}px`,
+          flexDirection: direction,
+          justifyContent: horizontalAlign,
+          alignContent: verticalAlign,
+          alignItems: verticalAlign,
+        }}
+        {...rest}
+      >
+        {children}
+      </div>
+    );
+  }
+
+  return (
+    <React.Fragment />
+  );
+};
+
+Col.propTypes = {
+  className: PropTypes.string,
+  cellSpacing: PropTypes.number,
+  rowSpacing: PropTypes.number,
+  children: PropTypes.node.isRequired,
+  direction: PropTypes.string,
+  horizontalAlign: PropTypes.string,
+  verticalAlign: PropTypes.string,
+  show: PropTypes.bool,
+  xs: PropTypes.number,
+  md: PropTypes.number,
+  sm: PropTypes.number,
+  lg: PropTypes.number,
+};
+
+Col.defaultProps = {
+  xs: 12,
+  md: 6,
+  sm: 4,
+  lg: 3,
+  show: true,
+  className: '',
+  cellSpacing: 0,
+  rowSpacing: 0,
+  direction: 'row',
+  horizontalAlign: 'flex-start',
+  verticalAlign: 'flex-start',
+};
+
+export default Col;
---
+++ src/components/Grid/Row/index.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Row = (props) => {
+  const {
+    children,
+    direction,
+    horizontalAlign,
+    verticalAlign,
+    cellSpacing,
+    rowSpacing,
+    className,
+    ...rest
+  } = props;
+
+  function childrenWithProps(item, ...restProps) {
+    return React.Children.map(item, child => React.cloneElement(child, ...restProps));
+  }
+
+  return (
+    <div
+      className={`grid-row ${className}`}
+      style={{
+        flexDirection: direction,
+        justifyContent: horizontalAlign,
+        alignContent: verticalAlign,
+        alignItems: verticalAlign,
+        width: `calc(100% + (${cellSpacing * 2}px))`,
+        marginRight: `-${cellSpacing}px`,
+        marginLeft: `-${cellSpacing}px`,
+        marginTop: `-${rowSpacing}px`,
+      }}
+      {...rest}
+    >
+      {childrenWithProps(children, { rowSpacing, cellSpacing })}
+    </div>
+  );
+};
+
+Row.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+  direction: PropTypes.string,
+  horizontalAlign: PropTypes.string,
+  verticalAlign: PropTypes.string,
+  cellSpacing: PropTypes.number,
+  rowSpacing: PropTypes.number,
+};
+
+Row.defaultProps = {
+  className: '',
+  direction: 'row',
+  horizontalAlign: 'flex-start',
+  verticalAlign: 'flex-start',
+  cellSpacing: 0,
+  rowSpacing: 0,
+};
+
+export default Row;
