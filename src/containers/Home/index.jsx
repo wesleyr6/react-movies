f92af333cb524212ad038dd12105c28a0c34968d
Wesley Amaro <wesley.biagi@avantica.net>
2019-08-12 00:08:32 -0500

    styles

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,28 +1,104 @@
 import React from 'react';
-import logo from '../../assets/images/logo.svg';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Header from '../../components/Header';
+import Banner from '../../components/Banner';
+import { Row, Col } from '../../components/Grid';
+import Cards from '../../components/Cards';
+import { getDiscoveryMovies } from '../../actions/movies';
 import './index.sass';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {'Edit '}
-          <code>src/App.js</code>
-          {' and save to reload.'}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Home extends React.Component {
+  state = {
+    discoveryMoviesLoading: true,
+  }
+
+  componentDidMount() {
+    this.discoveryMovies();
+  }
+
+  componentDidUpdate(prevProps) {
+    const { discoveryMovies } = this.props;
+
+    if (prevProps.discoveryMovies.length !== discoveryMovies.length) {
+      this.onHandleChange('discoveryMoviesLoading', false);
+    }
+  }
+
+  discoveryMovies = () => {
+    const {
+      discoveryMovies,
+      getDiscoveryMovies: getDiscoveryMoviesAction,
+    } = this.props;
+
+    if (discoveryMovies.length <= 0) {
+      getDiscoveryMoviesAction();
+    } else {
+      this.setState({
+        discoveryMoviesLoading: false,
+      });
+    }
+  }
+
+  onHandleChange = (name, value) => {
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    const { discoveryMovies } = this.props;
+    const { discoveryMoviesLoading } = this.state;
+
+    return (
+      <React.Fragment>
+        <Header />
+        <Banner />
+
+        <main>
+          <div className="wrapper">
+            <section className="featured-videos">
+              <h1>Suggested movies</h1>
+              {
+                !discoveryMoviesLoading && discoveryMovies.length > 0 ? (
+                  <Row rowSpacing={15} cellSpacing={15}>
+                    {
+                      discoveryMovies.map(item => (
+                        <Col
+                          key={item.id}
+                          lg={2}
+                          md={3}
+                          sm={6}
+                          xs={12}
+                        >
+                          <Cards item={item} />
+                        </Col>
+                      ))
+                    }
+                  </Row>
+                ) : (
+                  <React.Fragment>
+                    loading...
+                  </React.Fragment>
+                )
+              }
+            </section>
+          </div>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
-export default App;
+Home.propTypes = {
+  getDiscoveryMovies: PropTypes.func.isRequired,
+  discoveryMovies: PropTypes.instanceOf(Array),
+};
+
+Home.defaultProps = {
+  discoveryMovies: [],
+};
+
+const mapStateToProps = state => ({
+  discoveryMovies: state.movies.discoveryMovies,
+});
+
+export default connect(mapStateToProps, { getDiscoveryMovies })(Home);
