a4653c1369220c0eb9a3d9663ffedb2fd63a8d9e
Wesley Amaro <wesley.biagi@avantica.net>
2019-08-12 00:06:46 -0500

    feat: movies card

---
+++ src/components/Cards/index.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import moment from 'moment';
+import Rating from 'react-rating';
+import noImageIMG from '../../assets/images/no-image-available.jpeg';
+import './index.sass';
+
+const Cards = (props) => {
+  const { item } = props;
+
+  return (
+    <div className="cards" title={item.title}>
+      <figure className="cards-image">
+        {
+          item.backdrop_path ? (
+            <img src={`https://image.tmdb.org/t/p/w500/${item.backdrop_path}`} alt={item.title} />
+          ) : (
+            <img src={noImageIMG} alt={item.title} />
+          )
+        }
+
+      </figure>
+
+      <div className="cards-content">
+        <h2 className="cards-title">{item.title}</h2>
+        <Rating
+          stop={10}
+          emptySymbol={<i className="icon-star-o" />}
+          fullSymbol={<i className="icon-star" />}
+          initialRating={item.vote_average}
+          readonly
+          className="cards-rating"
+        />
+        <p className="cards-description">
+          {moment(new Date(item.release_date)).format('YYYY')}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+Cards.propTypes = {
+  item: PropTypes.instanceOf(Object).isRequired,
+};
+
+export default Cards;
