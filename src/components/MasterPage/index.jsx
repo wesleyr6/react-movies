428eef61861457d4765d057f00fb3a3a178f9be0
Wesley Amaro <wesley.biagi@avantica.net>
2019-08-12 00:06:29 -0500

    feat: masterpage

---
+++ src/components/MasterPage/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Header from '../Header';
+
+const MasterPage = ({ children }) => (
+  <React.Fragment>
+    <Header />
+
+    <main>
+      {children}
+    </main>
+  </React.Fragment>
+);
+
+MasterPage.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default MasterPage;
