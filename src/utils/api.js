9e5420a376d9ff9c51b0e10cc446405a90400de1
Wesley Amaro <wesley.biagi@avantica.net>
2019-08-12 00:09:19 -0500

    feat: API

---
+++ src/utils/api.js
@@ -0,0 +1,51 @@
+import axios from 'axios';
+import { REACT_APP_API_URL, REACT_APP_API_KEY } from '../config/variables';
+
+export const API = REACT_APP_API_URL;
+
+const headers = {
+  Accept: 'application/json',
+  'Content-Type': 'application/json',
+};
+
+export const APIResquest = async (config) => {
+  const requestURI = `${REACT_APP_API_URL}/${config.uri}`;
+
+  const requestConfig = () => {
+    const settings = {
+      method: config.method,
+      headers: {
+        ...headers,
+        ...config.headers || null,
+      },
+      params: {
+        ...config.params,
+        api_key: REACT_APP_API_KEY,
+      },
+    };
+
+    if (config.method === 'POST' || config.method === 'PUT') {
+      settings.data = JSON.stringify(config.data || {});
+    }
+
+    return settings;
+  };
+
+  const promiseResquestAPI = async () => {
+    try {
+      const { data } = await axios(requestURI, requestConfig());
+      return data;
+    } catch (err) {
+      const errorMessage = 'Something went wrong.';
+      if (err.response.data && err.response.data.status_message) {
+        throw err.response.status_message;
+      }
+
+      throw errorMessage;
+    }
+  };
+
+  return promiseResquestAPI();
+};
+
+export default { API, APIResquest };
