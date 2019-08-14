00aafdccdb42dc14371736a9625245e153c7fb79
Wesley Amaro <wesley.biagi@avantica.net>
2019-08-13 19:59:54 -0500

    styles

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import moment from 'moment';
 import Rating from 'react-rating';
 import noImageIMG from '../../assets/images/no-image-available.jpeg';
@@ -10,31 +11,33 @@ const Cards = (props) => {
 
   return (
     <div className="cards" title={item.title}>
-      <figure className="cards-image">
-        {
-          item.backdrop_path ? (
-            <img src={`https://image.tmdb.org/t/p/w500/${item.backdrop_path}`} alt={item.title} />
-          ) : (
-            <img src={noImageIMG} alt={item.title} />
-          )
-        }
+      <Link to={`/${item.id}`}>
+        <figure className="cards-image">
+          {
+            item.backdrop_path ? (
+              <img src={`https://image.tmdb.org/t/p/w500/${item.backdrop_path}`} alt={item.title} />
+            ) : (
+              <img src={noImageIMG} alt={item.title} />
+            )
+          }
 
-      </figure>
+        </figure>
 
-      <div className="cards-content">
-        <h2 className="cards-title">{item.title}</h2>
-        <Rating
-          stop={10}
-          emptySymbol={<i className="icon-star-o" />}
-          fullSymbol={<i className="icon-star" />}
-          initialRating={item.vote_average}
-          readonly
-          className="cards-rating"
-        />
-        <p className="cards-description">
-          {moment(new Date(item.release_date)).format('YYYY')}
-        </p>
-      </div>
+        <div className="cards-content">
+          <h2 className="cards-title">{item.title}</h2>
+          <Rating
+            stop={10}
+            emptySymbol={<i className="icon-star-o" />}
+            fullSymbol={<i className="icon-star" />}
+            initialRating={item.vote_average}
+            readonly
+            className="cards-rating"
+          />
+          <p className="cards-description">
+            {moment(new Date(item.release_date)).format('YYYY')}
+          </p>
+        </div>
+      </Link>
     </div>
   );
 };
