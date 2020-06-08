41fbebe36616539dea73c99bddf027932e70cbe5
Wesley Amaro <contato@wesleyamaro.com>
2020-06-08 16:56:12 -0500

    refactor: hooks

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,98 +1,66 @@
 /* eslint-disable no-undef */
-import React from 'react';
-import PropTypes from 'prop-types';
-import queryString from 'query-string';
-import { withRouter, Link } from 'react-router-dom';
-import { Row, Col } from '../Grid';
-import './index.sass';
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import queryString from "query-string";
+import { withRouter, Link } from "react-router-dom";
+import { Row, Col } from "../Grid";
+import "./index.sass";
 
-class Header extends React.Component {
-  state = {
-    search: '',
-  }
+const Header = ({ history, location }) => {
+  const [search, setSearch] = useState("");
 
-  componentDidMount() {
-    this.searchQueryString();
-  }
+  useEffect(() => {
+    searchQueryString();
+  }, [location.search]);
 
-  searchQueryString = () => {
-    const { history } = this.props;
-    const { search } = this.state;
-    const parsed = queryString.parse(history.location.search);
+  const searchQueryString = () => {
+    const parsed = queryString.parse(location.search);
 
     if (parsed && parsed.movie && parsed.movie !== search) {
-      this.setState({
-        search: parsed.movie,
-      });
+      setSearch(parsed.movie);
     }
-  }
+  };
 
-  onHandleChange = (name, value) => {
-    this.setState({
-      [name]: value,
-    });
-  }
-
-  onHandleSubmit = (e) => {
+  const onHandleSubmit = (e) => {
     e.preventDefault();
-
-    const { search } = this.state;
-    const { history } = this.props;
-
     history.push(`/search?movie=${search}`);
-  }
+  };
 
-  render() {
-    const { search } = this.state;
+  return (
+    <header className="header">
+      <div className="wrapper">
+        <Row
+          rowSpacing={5}
+          cellSpacing={10}
+          horizontalAlign="space-between"
+          verticalAlign="center"
+        >
+          <Col lg={6} md={6} sm={6} xs={12}>
+            <div className="header-logo">
+              <Link to="/">myMovies</Link>
+            </div>
+          </Col>
 
-    return (
-      <header className="header">
-        <div className="wrapper">
-          <Row
-            rowSpacing={5}
-            cellSpacing={10}
-            horizontalAlign="space-between"
-            verticalAlign="center"
-          >
-            <Col
-              lg={6}
-              md={6}
-              sm={6}
-              xs={12}
-            >
-              <div className="header-logo">
-                <Link to="/">myMovies</Link>
-              </div>
-            </Col>
-
-            <Col
-              lg={6}
-              md={6}
-              sm={6}
-              xs={12}
-            >
-              <form className="header-search" onSubmit={e => this.onHandleSubmit(e)}>
-                <input
-                  type="text"
-                  name="search"
-                  autoComplete="off"
-                  className="full"
-                  placeholder="search movie..."
-                  value={search}
-                  onChange={({ target }) => this.onHandleChange(target.name, target.value)}
-                  required
-                />
-                <button type="submit">
-                  search
-                </button>
-              </form>
-            </Col>
-          </Row>
-        </div>
-      </header>
-    );
-  }
-}
+          <Col lg={6} md={6} sm={6} xs={12}>
+            <form className="header-search" onSubmit={(e) => onHandleSubmit(e)}>
+              <input
+                type="text"
+                name="search"
+                autoComplete="off"
+                className="full"
+                placeholder="search movie..."
+                value={search}
+                onChange={({ target }) => setSearch(target.value)}
+                required
+              />
+              <button type="submit">search</button>
+            </form>
+          </Col>
+        </Row>
+      </div>
+    </header>
+  );
+};
 
 Header.propTypes = {
   history: PropTypes.instanceOf(Object).isRequired,
